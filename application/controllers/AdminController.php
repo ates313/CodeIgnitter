9321ac60684597c0b1f5d952b3913149db7b80d3
Ates06 <ates.qurbanov06@gmail.com>
2023-04-24 00:45:14 +0400

    CodeIgnitter dating

---
+++ application/controllers/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+class AdminController extends CI_Controller{
+    public function index(){
+        echo "salam men index";
+    }
+
+    public function about(){
+        echo "salam men about";
+    }
+
+    
+
+
+
+
+
+}
